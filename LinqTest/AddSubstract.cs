4ac7ed5be9d4ef089573998042c3db2f37b16c52
Bartek <bartlomiej.kacki@gmail.com>
2024-07-14 15:44:59 +0200

    Finished exercise

---
+++ LinqTest/AddSubstract.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqTest
+{
+    internal class AddSubstract
+    {
+        public int Value { get; set; }
+
+        public AddSubstract Add(int i)
+        {
+            Console.WriteLine($"Wartość: {Value}, dodawanie {i}");
+            return new AddSubstract() { Value = Value + i };
+        }
+
+        public AddSubstract Substract(int i)
+        {
+            Console.WriteLine($"Wartość: {Value}, odejmowanie {i}");
+            return new AddSubstract() { Value = Value - i};
+        }
+    }
+}
--- LinqTest/Program.cs
+++ LinqTest/Program.cs
@@ -8,10 +8,30 @@
             List<int> numbers = new List<int>();
             for(int i = 1; i<=99; i++)
                 numbers.Add(i);
-            IEnumerable<int> firstAndLastFive = numbers.Take(5).Concat(numbers.TakeLast(5));
+            IEnumerable<int> firstAndLastFive = numbers.Take(5).Concat(numbers.TakeLast(5)); //łańcuch metod
 
             foreach (int i in firstAndLastFive)
                 Console.Write($"{i} ");
+
+            Console.WriteLine();
+
+            int[] values = new int[] { 0, 12, 44, 36, 92, 54, 13, 8 };
+            IEnumerable<int> result =
+                from v in values
+                where v < 37
+                orderby -v
+                select v;
+            foreach(int i in result)
+                Console.Write($"{i} ");
+
+            Console.WriteLine();
+
+            AddSubstract a = new AddSubstract() { Value = 5 }
+            .Add(5)
+            .Substract(3)
+            .Add(9)
+            .Substract(12);
+            Console.WriteLine($"Wynik: {a.Value}");
         }
     }
 }
